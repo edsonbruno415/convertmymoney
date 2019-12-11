763749b718af312d38776e8da0bdd8067140dbde
Edson Bruno <edsonbruno415@gmail.com>
2019-12-11 01:01:47 -0300

    integrando api do Banco do Brasil ao projeto.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/apiBCB')
 
 const port = process.env.PORT || 3000
 
@@ -9,8 +10,12 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    console.log(cotacao)
+    res.render('home',{
+        cotacao
+    })
 })
 
 app.get('/cotacao', (req, res) => {
